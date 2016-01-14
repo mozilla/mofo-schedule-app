3028f213f292ed47ef6a157f3afd173970af831d
test <test@localhost.localdomain>
2016-01-14 17:35:48 -0500

    attempting to get travis to build gh-pages

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+git config user.name "Travis CI"
+
+git subtree push --prefix public "https://${GH_TOKEN}@github.com/mozilla/mofo-schedule-app.git" master:gh-pages
