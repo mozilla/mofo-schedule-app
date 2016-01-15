f2f2cfb7219cdcedb5be6c9b40c3276776b840d3
ScottDowne <scott.downe@gmail.com>
2016-01-14 19:13:03 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,4 @@
 
 set -o errexit -o nounset
 
-rev=$(git rev-parse --short HEAD)
-
-git config user.name "Travis CI"
-
 git subtree push --prefix public "https://${GH_TOKEN}@github.com/mozilla/mofo-schedule-app.git" gh-pages
