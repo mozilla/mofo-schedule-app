50476648e9ce73b1845bc426f699bef7618f2ce3
ScottDowne <scott.downe@gmail.com>
2016-01-18 01:12:37 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,4 @@
 
 set -o errexit -o nounset
 
-git remote add upstream "https://${GH_TOKEN}@github.com/mozilla/mofo-schedule-app.git"
-git pull upstream master
-
-git subtree push --prefix public upstream gh-pages
+git push "https://${GH_TOKEN}@github.com/mozilla/mofo-schedule-app.git" `git subtree split --prefix public`:gh-pages --force
