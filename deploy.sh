d2d05e7f0b508b5eb34d6da11353f472bbc00cbd
ScottDowne <scott.downe@gmail.com>
2016-01-18 00:26:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 set -o errexit -o nounset
 
 git remote add upstream "https://${GH_TOKEN}@github.com/mozilla/mofo-schedule-app.git"
-git fetch upstream
-git checkout upstream/master
+git pull upstream master
 
 git subtree push --prefix public upstream gh-pages
